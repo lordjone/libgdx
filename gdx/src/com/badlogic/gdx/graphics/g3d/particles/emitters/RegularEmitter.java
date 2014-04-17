e7e8d1ed10f19341e66a5ef5dac5d3d4b082b0d6
przemyslawlis <lisprzemyslaw84@gmail.com>
2014-04-17 05:05:03 +0200

    Additional flag added for continous emitters for pausing and resuming emission.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/emitters/RegularEmitter.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/emitters/RegularEmitter.java
@@ -18,7 +18,7 @@ public class RegularEmitter<T extends Particle> extends Emitter<T> implements Js
 	protected int lifeOffset, lifeOffsetDiff;
 	protected int life, lifeDiff;
 	protected float duration = 1, delay, durationTimer, delayTimer;
-	private boolean continuous;
+	private boolean continuous, emissionCycleRunning;
 
 	public RegularEmitter(){
 		delayValue = new RangedNumericValue(); 
@@ -31,6 +31,7 @@ public class RegularEmitter<T extends Particle> extends Emitter<T> implements Js
 		emissionValue.setActive(true);
 		lifeValue.setActive(true);
 		continuous = true;
+		emissionCycleRunning = true;
 	}
 	
 	public RegularEmitter (RegularEmitter regularEmitter) {
@@ -91,7 +92,7 @@ public class RegularEmitter<T extends Particle> extends Emitter<T> implements Js
 		if (delayTimer < delay) {
 			delayTimer += deltaMillis;
 		} else {
-			boolean isEmissionCycleRunning = true;
+			boolean emitNextCycle = emissionCycleRunning;
 			//End check
 			if (durationTimer < duration) {
 				durationTimer += deltaMillis;
@@ -101,10 +102,10 @@ public class RegularEmitter<T extends Particle> extends Emitter<T> implements Js
 				if (continuous) 
 					controller.start();
 				else 
-					isEmissionCycleRunning = false;
+					emitNextCycle = false;
 			}
 			
-			if(isEmissionCycleRunning) {
+			if(emitNextCycle) {
 				//Emit particles
 				emissionDelta += deltaMillis;
 				float emissionTime = emission + emissionDiff * emissionValue.getScale(percent);
@@ -183,6 +184,14 @@ public class RegularEmitter<T extends Particle> extends Emitter<T> implements Js
 
 	public void setContinuous (boolean continuous) {
 		this.continuous = continuous;
+	}
+	
+	public boolean isEmissionCycleRunning(){
+		return emissionCycleRunning;
+	}
+	
+	public void setEmissionCycleRunning(boolean emissionCycleRunning){
+		this.emissionCycleRunning = emissionCycleRunning;
 	}
 	
 	public boolean isComplete () {
